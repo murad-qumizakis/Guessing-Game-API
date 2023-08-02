3f776fbaa018788020b00f01b1b227409129e729
Murad Qumizakis <muradqumizakis@gmail.com>
2023-08-02 17:48:43 -0500

    Comitting guessing game API

---
+++ database.js
@@ -0,0 +1,44 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.MYSQL_PORT,
+  })
+  .promise();
+
+async function getAllGames() {
+  let result = await pool.query("SELECT * FROM game");
+  return result[0];
+}
+
+async function getGameById(id) {
+  let result = await pool.query("SELECT * FROM game WHERE id = ?", [id]);
+  return result[0];
+}
+
+async function createGame(name) {
+  let randomNumber = Math.floor(Math.random() * 10000);
+  try {
+    let [result] = await pool.query(
+      "INSERT INTO game (name, number) VALUES (?, ?)",
+      [name, randomNumber]
+    );
+    return getGameById(result.insertId);
+  } catch (err) {
+    console.log("error: ", err);
+    return;
+  }
+}
+
+async function deleteGame(id) {
+  await pool.query("DELETE FROM game WHERE id = ?", [id]);
+}
+
+export { getAllGames, getGameById, createGame, deleteGame };
---
+++ server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import {
+  getAllGames,
+  getGameById,
+  createGame,
+  deleteGame,
+} from "./database.js";
+
+const app = express();
+app.use(express.json());
+
+const port = 3001;
+
+const checkTokenMiddleware = (req, res, next) => {
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  const token = authorizationHeader.split(" ")[1];
+
+  if (token !== process.env.TOKEN) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  next();
+};
+
+app.use(checkTokenMiddleware);
+
+app.get("/", async (req, res) => {
+  const games = await getAllGames();
+  res.json(games);
+});
+
+app.get("/game/:id", async (req, res) => {
+  const { id } = req.params;
+  const game = await getGameById(id);
+  res.send(game);
+});
+
+app.post("/game", async (req, res) => {
+  const { name } = req.body;
+  const [newGame] = await createGame(name);
+  res.json(newGame);
+});
+
+app.delete("/game/:id", async (req, res) => {
+  const { id } = req.params;
+  await deleteGame(id);
+  res.send("Game deleted");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
